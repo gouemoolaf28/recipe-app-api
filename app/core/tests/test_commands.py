5533b4bd5845bdf039d32896d2b6fb89997dd008
gouemoolaf28 <gouemoolaf28@gmail.com>
2026-05-30 22:59:58 +0200

    Correct Unit Test

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -24,7 +24,7 @@ class CommandTests(TestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2Error] * 3 + [OperationalError] \
             * 3 + [True]
